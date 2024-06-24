d19e7ec155dd723e85d77b90a477a97f31df724c
Wiayass <77987013+Wiayass@users.noreply.github.com>
2024-06-24 19:56:37 +0500

    index.js

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const elements = document.querySelectorAll('.element');
-    const box = document.querySelectorAll('.box');
+  const elements = document.querySelectorAll('.element');
   
-    // Function to add 'visible' class to elements
-    const showElements1 = () => {
-      elements.forEach(element => {
-        const rect = element.getBoundingClientRect();
-        if (rect.top < window.innerHeight - 100) {
-          element.classList.add('visible');
-        }
-      });
-    };
-  
-    // Show elements on page load
-    showElements1();
-  
-    // Show elements on scroll
-    window.addEventListener('scroll', showElements1);
+
+  // Function to add 'visible' class to elements first
+  const showElements1 = () => {
+    elements.forEach(element => {
+      const rect = element.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 100) {
+        element.classList.add('visible');
+      }
+    });
+  };
+
+  // Show elements on page load
+  showElements1();
+
+  // Show elements on scroll
+  window.addEventListener('scroll', showElements1);
 
 
+  const box = document.querySelectorAll('.box');
 
-    const showElements2 = () => {
-        box.forEach(element => {
-          const rect = element.getBoundingClientRect();
-          if (rect.top < window.innerHeight - 100) {
-            element.classList.add('visible');
-          }
-        });
-      };
-    
-      // Show elements on page load
-      showElements2();
-    
-      // Show elements on scroll
-      window.addEventListener('scroll', showElements2);
-  });
-  
+  // Function to add 'visible' class to elements second
+  const showElements2 = () => {
+    box.forEach(element => {
+      const rect = element.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 100) {
+        element.classList.add('visible');
+      }
+    });
+  };
+
+  // Show elements on page load
+  showElements2();
+
+  // Show elements on scroll
+  window.addEventListener('scroll', showElements2);
+});
